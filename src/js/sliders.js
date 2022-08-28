e9ea400fd980d533da1674efcc6c50faf2813d26
arichild <arieevq@gmail.com>
2022-08-29 01:19:48 +0300

    save

--- src/js/sliders.js
+++ src/js/sliders.js
@@ -11,3 +11,10 @@ const swiper = new Swiper('.welcome', {
     prevEl: '.swiper-button-prev',
   },
 });
+
+// We listen to the resize event
+window.addEventListener('resize', () => {
+  // We execute the same script as before
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
